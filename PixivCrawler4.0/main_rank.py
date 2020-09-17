fd548eeee8681be692bc8ad042876ce0261cdb9c
MayaSHM <55118867+MayaSHM@users.noreply.github.com>
2020-09-17 19:30:30 +0800

    Create main_rank.py

---
+++ PixivCrawler4.0/main_rank.py
@@ -0,0 +1,17 @@
+from PixivCrawler4 import PixivRankCrawler
+
+
+def main():
+    """
+        mode: 默认daily
+        'daily': 每日 'weekly': 每周 'male': 受男性欢迎 'female': 受女性欢迎  'rookie'：新人  'original'：原创
+
+        date：默认前一天。格式：'yyyymmdd'
+    """
+    crawl = PixivRankCrawler(mode='daily')
+    crawl.run()
+    pass
+
+
+if __name__ == '__main__':
+    main()
