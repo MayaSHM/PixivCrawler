dc3283da5a05ae55891d5b2992414cc6957233cf
MayaSHM <55118867+MayaSHM@users.noreply.github.com>
2020-09-17 19:36:01 +0800

    Update main_rank.py

--- PixivCrawler4.0/main_rank.py
+++ PixivCrawler4.0/main_rank.py
@@ -3,8 +3,11 @@ from PixivCrawler4 import PixivRankCrawler
 
 def main():
     """
+        PixivRankCrawler所需参数：
+        
         mode: 默认daily
-        'daily': 每日 'weekly': 每周 'male': 受男性欢迎 'female': 受女性欢迎  'rookie'：新人  'original'：原创
+        'daily': 每日 'weekly': 每周 'monthly'：本月
+        'male': 受男性欢迎 'female': 受女性欢迎  'rookie'：新人  'original'：原创
 
         date：默认前一天。格式：'yyyymmdd'
     """
